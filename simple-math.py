8e33226739709d4c015a2b9ec8fe023a95738a90
Raiwin Inthasit <andaraiwin@gmail.com>
2021-09-15 18:07:33 +0700

    add multiply

--- simple-math.py
+++ simple-math.py
@@ -1,16 +1,14 @@
 def add(a, b):
-    return(a+b)
-def subtract (b, a):
-    return (b - a)
-    
-def divide(b, a):
-    return (b/a)
+    return(a + b)
+
+def multiply(a, b):
+    return(a*b)
 
 i = 0
 
-i = add (i, 3)
+i = add(i, 3)
 i = multiply(i, 5)
 i = subtract(i, 1)
-i = divide(i,2)
+i = divide(i, 2)
 
 print("i = " + str(i))
